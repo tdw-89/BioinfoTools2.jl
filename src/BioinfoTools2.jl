046f6279ba84a4bd996a5dd6d0bc2265636ab141
Tom Wolfe <Thomas_Wolfe@student.uml.edu>
2026-07-05 10:33:08 -0400

    top level re-exports sub-modules

--- src/BioinfoTools2.jl
+++ src/BioinfoTools2.jl
@@ -2,7 +2,13 @@ module BioinfoTools2
     include("reference.jl")
     include("studies.jl")
     include("paralogs.jl")
+    
+    using .Reference
+    using .Studies
     using .Paralogs
 
-    export Paralogs
+    export
+        Studies,
+        Reference,
+        Paralogs
 end
